3430f5e0e0f875522f434355097452c043fbc4b0
IbnuAhsani <ibnu.muhari@gmail.com>
2019-10-29 10:50:08 +0700

    feat: add to homescreen

--- sw.js
+++ sw.js
@@ -1,18 +1,33 @@
 var cacheName = "hello-pwa";
 var filesToCache = ["/", "./index.html", "./css/style.css", "./js/main.js"];
 
-self.addEventListener("install", function(e) {
+self.addEventListener("install", e => {
   e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
+    caches.open(cacheName).then(cache => {
       console.log("service worker installed");
       return cache.addAll(filesToCache);
     })
   );
 });
 
-self.addEventListener("fetch", function(e) {
+self.addEventListener("activate", e => {
+  console.log("service worker activate");
+
+  e.waitUntil(
+    caches.keys().then(keyList => {
+      if (key !== cacheName) {
+        console.log("service worker removing old caches", key);
+        return caches.delete(key);
+      }
+    })
+  );
+
+  return self.clients.claim();
+});
+
+self.addEventListener("fetch", e => {
   e.respondWith(
-    caches.match(e.request).then(function(response) {
+    caches.match(e.request).then(response => {
       console.log("service worker fetched");
       return response || fetch(e.request);
     })
